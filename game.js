3524c847f19b09738ba28cdfbac82f3c6ce429c9
Alex Lacy <Alex Lacy>
2016-05-17 22:15:17 -0700

    Large update to targets motion

--- game.js
+++ game.js
@@ -56,6 +56,17 @@ t2.interactive = true;
 t2.on('mousedown', clear_sprite.bind(null, t2));
 
 
+// Adds the third target
+targets.addChild(t3);
+t3.position.x = 500;
+t3.position.y = 450;
+t3.anchor.x = 0.9;
+t3.anchor.y = 0.9;
+t3.interactive = true;
+
+t3.on('mousedown', clear_sprite.bind(null, t3));
+
+
 
 
 
@@ -76,6 +87,8 @@ var togglet1x = 1; // Saves a state change for whether targets are moving right 
 var togglet1y = 1;
 var togglet2x = 1;
 var togglet2y = 1;
+var togglet3x = 1;
+var togglet3y = 1;
 
 
 function animate() {
@@ -116,7 +129,7 @@ function animate() {
 		}
 	}
 
-
+	t1.rotation += .05;
 
 
 
@@ -138,7 +151,7 @@ function animate() {
 		}
 	}
 
-	// t1 y movement 
+	// t2 y movement 
 
 	if(togglet2y == 1){
 		
@@ -150,19 +163,67 @@ function animate() {
 	}
 
 	else{
-		t2.position.y -= 10;
+		t2.position.y -= 25;
 		if(t2.position.y == 50){
 			togglet2y = 1;
 		}
 	}
 	
-	if(numdown == 2){
+
+
+	// Creates the movement patterns for the third target
+	// Adds x movement
+	if(togglet3x == 1){
+		
+		t3.position.x += 10;
+
+		if(t3.position.x == 1700){
+			togglet3x = 0;
+		}
+	}
+
+	else{
+		t3.position.x -= 20;
+		if(t3.position.x == 100){
+			togglet3x = 1;
+		}
+	}
+
+	// t3 y movement 
+
+	if(togglet3y == 1){
+		
+		t3.position.y += 25;
+
+		if(t3.position.y == 500){
+			togglet3y = 0;
+		}
+	}
+
+	else{
+		t3.position.y -= 10;
+		if(t3.position.y == 100){
+			togglet3y = 1;
+		}
+	}
+
+	t3.rotation += .14;
+
+
+
+
+
+	// Adds the victory condition, and causes the victory text to display
+	if(numdown == 3){
 		winscreen.renderable = true;
 	}
 
 	renderer.render(stage);
 
 }
+
+
+
 
 
 animate();
